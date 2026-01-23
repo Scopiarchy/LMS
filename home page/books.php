e466a6ef917891389cd13218ba5af6c160aa1129
malitha-tishamal <malithatishamal@gmail.com>
2026-01-23 09:49:08 +0530

    add files of front end
    models, footer ,all pages

---
+++ home page/books.php
@@ -0,0 +1,8 @@
+﻿<?php include 'includes/header.php'; ?>
+
+<main>
+    <?php include 'includes/books.php'; ?>
+</main>
+
+<?php include 'includes/footer.php'; ?>
+<?php include 'includes/footerlinkinc.php'; ?>
